aa367d5f460370c2c8e9521b5d8f0033266bccd1
Aslam Ahammed <aslamplr@users.noreply.github.com>
2020-04-16 17:35:48 +0530

    Refactor and minor ergonomics (#2)

--- src/main.rs
+++ src/main.rs
@@ -78,31 +78,70 @@ async fn main() -> Result<()> {
   let repo_name = matches.value_of("repo_name").unwrap();
   let auth_token = matches.value_of("auth_token").unwrap();
 
+  let action = matches.value_of("action");
   let secret_key = matches.value_of("secret_key");
   let secret_value = matches.value_of("secret_value");
 
-  match (matches.value_of("action"), secret_key, secret_value) {
+  let repo = ReposRequestParams::new(repo_owner, repo_name, auth_token);
+
+  match (action, secret_key, secret_value) {
     (Some("list"), _, _) => {
-      let secret_list = get_secret_list(repo_owner, repo_name, auth_token).await?;
-      println!("Secret List:\n\n{:#?}", secret_list);
+      let secret_list = repo.get_secret_list().await?;
+      println!("All Secrets:\n\n{}", secret_list);
     }
     (Some("get"), Some(secret_key), _) => {
-      let secret = get_a_secret(repo_owner, repo_name, auth_token, secret_key).await?;
-      println!("Secret:\n\n{:#?}", secret);
+      let secret = repo.get_a_secret(&secret_key).await?;
+      println!("Secret:\n\n{}", secret);
     }
     (Some("add"), Some(secret_key), Some(secret_value))
     | (Some("edit"), Some(secret_key), Some(secret_value)) => {
-      let public_key = get_public_key(repo_owner, repo_name, auth_token).await?;
-      SecretSaveRequest::from(secret_key, secret_value, public_key)?
-        .make_api_call(repo_owner, repo_name, auth_token)
-        .await?;
+      repo.save_secret(secret_key, secret_value).await?;
     }
     (Some("delete"), Some(secret_key), _) => {
-      delete_a_secret(repo_owner, repo_name, auth_token, secret_key).await?;
+      repo.delete_a_secret(secret_key).await?;
     }
     _ => {}
   }
   Ok(())
+}
+
+struct Repo<'a> {
+  repo_owner: &'a str,
+  repo_name: &'a str,
+}
+
+struct ReposRequestParams<'a>(Repo<'a>, &'a str);
+
+impl<'a> ReposRequestParams<'a> {
+  fn new(repo_owner: &'a str, repo_name: &'a str, auth_token: &'a str) -> Self {
+    let repo = Repo {
+      repo_owner: &repo_owner,
+      repo_name: &repo_name,
+    };
+    ReposRequestParams(repo, &auth_token)
+  }
+
+  async fn get_public_key(&self) -> Result<PublicKeyResponse> {
+    get_public_key(&self).await
+  }
+
+  async fn get_secret_list(&self) -> Result<SecretListResponse> {
+    get_secret_list(&self).await
+  }
+
+  async fn get_a_secret(&self, secret_key: &str) -> Result<Secret> {
+    get_a_secret(&self, &secret_key).await
+  }
+
+  async fn save_secret(&self, secret_key: &str, secret_value: &str) -> Result<()> {
+    let public_key = self.get_public_key().await?;
+    let secret_save_req = SecretSaveRequest::from(secret_key, secret_value, public_key)?;
+    secret_save_req.make_api_call(&self).await
+  }
+
+  async fn delete_a_secret(&self, secret_key: &str) -> Result<()> {
+    delete_a_secret(&self, secret_key).await
+  }
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -128,18 +167,21 @@ impl SecretSaveRequest {
     })
   }
 
-  async fn make_api_call(&self, repo_owner: &str, repo_name: &str, auth_token: &str) -> Result<()> {
-    put_gh_secret(repo_owner, repo_name, auth_token, &self.key, &self).await
+  async fn make_api_call(&self, params: &ReposRequestParams<'_>) -> Result<()> {
+    put_gh_secret(&params, &self.key, &self).await
   }
 }
 
 async fn put_gh_secret(
-  repo_owner: &str,
-  repo_name: &str,
-  auth_token: &str,
+  params: &ReposRequestParams<'_>,
   secret_key: &str,
   secret_save_req: &SecretSaveRequest,
 ) -> Result<()> {
+  let ReposRequestParams(repo, auth_token) = params;
+  let Repo {
+    repo_owner,
+    repo_name,
+  } = repo;
   let uri = format!(
     "https://api.github.com/repos/{}/{}/actions/secrets/{}",
     repo_owner, repo_name, secret_key
@@ -160,15 +202,15 @@ async fn put_gh_secret(
   Ok(())
 }
 
-async fn get_from_gh<T>(
-  path: &str,
-  repo_owner: &str,
-  repo_name: &str,
-  auth_token: &str,
-) -> Result<T>
+async fn get_from_gh<T>(path: &str, params: &ReposRequestParams<'_>) -> Result<T>
 where
   T: serde::de::DeserializeOwned,
 {
+  let ReposRequestParams(repo, auth_token) = params;
+  let Repo {
+    repo_owner,
+    repo_name,
+  } = repo;
   let uri = format!(
     "https://api.github.com/repos/{}/{}/{}",
     repo_owner, repo_name, path
@@ -185,7 +227,7 @@ where
 
   let body = hyper::body::aggregate(res).await?;
 
-  let secret_list: T = serde_json::from_reader(body.reader())?;
+  let secret_list = serde_json::from_reader(body.reader())?;
 
   Ok(secret_list)
 }
@@ -197,19 +239,18 @@ struct Secret {
   updated_at: String,
 }
 
-async fn get_a_secret(
-  repo_owner: &str,
-  repo_name: &str,
-  auth_token: &str,
-  secret_key: &str,
-) -> Result<Secret> {
-  get_from_gh(
-    &format!("actions/secrets/{}", secret_key),
-    repo_owner,
-    repo_name,
-    auth_token,
-  )
-  .await
+impl std::fmt::Display for Secret {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(
+      f,
+      "{}",
+      serde_json::to_string_pretty(&self).unwrap_or_default()
+    )
+  }
+}
+
+async fn get_a_secret(params: &ReposRequestParams<'_>, secret_key: &str) -> Result<Secret> {
+  get_from_gh(&format!("actions/secrets/{}", secret_key), &params).await
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -218,12 +259,18 @@ struct SecretListResponse {
   secrets: Vec<Secret>,
 }
 
-async fn get_secret_list(
-  repo_owner: &str,
-  repo_name: &str,
-  auth_token: &str,
-) -> Result<SecretListResponse> {
-  get_from_gh("actions/secrets", repo_owner, repo_name, auth_token).await
+impl std::fmt::Display for SecretListResponse {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(
+      f,
+      "{}",
+      serde_json::to_string_pretty(&self).unwrap_or_default()
+    )
+  }
+}
+
+async fn get_secret_list(params: &ReposRequestParams<'_>) -> Result<SecretListResponse> {
+  get_from_gh("actions/secrets", &params).await
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -232,26 +279,16 @@ struct PublicKeyResponse {
   key: String,
 }
 
-async fn get_public_key(
-  repo_owner: &str,
-  repo_name: &str,
-  auth_token: &str,
-) -> Result<PublicKeyResponse> {
-  get_from_gh(
-    "actions/secrets/public-key",
-    repo_owner,
-    repo_name,
-    auth_token,
-  )
-  .await
+async fn get_public_key(params: &ReposRequestParams<'_>) -> Result<PublicKeyResponse> {
+  get_from_gh("actions/secrets/public-key", &params).await
 }
 
-async fn delete_a_secret(
-  repo_owner: &str,
-  repo_name: &str,
-  auth_token: &str,
-  secret_key: &str,
-) -> Result<()> {
+async fn delete_a_secret(params: &ReposRequestParams<'_>, secret_key: &str) -> Result<()> {
+  let ReposRequestParams(repo, auth_token) = params;
+  let Repo {
+    repo_owner,
+    repo_name,
+  } = repo;
   let uri = format!(
     "https://api.github.com/repos/{}/{}/actions/secrets/{}",
     repo_owner, repo_name, secret_key
@@ -279,10 +316,7 @@ fn create_https_client(
 
 fn create_request(auth_token: &str) -> hyper::http::request::Builder {
   Request::builder()
-    .header(
-      "Authorization",
-      format!("bearer {}", auth_token),
-    )
+    .header("Authorization", format!("bearer {}", auth_token))
     .header("User-Agent", "gh-actions-secrets-cli")
 }
 
@@ -292,10 +326,14 @@ fn seal(message: &str, public_key_base64: &str) -> Result<String> {
 
   let public_key = base64::decode(public_key_base64)?;
 
-  let mut public_key_buffer = [0u8; 32];
-  for (i, n) in public_key[..32].iter().enumerate() {
-    public_key_buffer[i] = *n;
-  }
+  let public_key_buffer =
+    public_key[..32]
+      .iter()
+      .enumerate()
+      .fold([0u8; 32], |mut buffer, (i, n)| {
+        buffer[i] = *n;
+        buffer
+      });
 
   let public_key = PublicKey::from(public_key_buffer);
 
