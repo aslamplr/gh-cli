79d0f2eb2d06521f5a2f627d10d1ecd74e9b8adf
Aslam Ahammed <aslamplr@gmail.com>
2020-05-16 05:05:26 +0530

    Make 📎 clippy happy 😁

--- src/utils/sealed_box.rs
+++ src/utils/sealed_box.rs
@@ -1,11 +1,10 @@
 use anyhow::{anyhow, Result};
-use base64;
 use sodiumoxide::crypto::{box_::curve25519xsalsa20poly1305::PublicKey, sealedbox};
 
 pub fn seal(message: &str, public_key_base64: &str) -> Result<String> {
     let public_key = base64::decode(public_key_base64)?;
-    let public_key =
-        PublicKey::from_slice(&public_key).ok_or(anyhow!("unable to create public key object"))?;
+    let public_key = PublicKey::from_slice(&public_key)
+        .ok_or_else(|| anyhow!("unable to create public key object"))?;
 
     let sealed_box = sealedbox::seal(message.as_bytes(), &public_key);
     let sealed_box_base64 = base64::encode(&sealed_box);
