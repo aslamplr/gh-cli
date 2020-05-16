2652d6e1687eeca1b05b17042e94fb40376b2806
Aslam Ahammed <aslamplr@gmail.com>
2020-05-17 00:03:28 +0530

    Fix: codegen output to OUT_DIR
    - codegen output to env:OUT_DIR.
    - Remove the src/graphql/repo_basic_info.rs from src/.
    - Kept src/graphql/mod.rs with include!(codegen generated files).

--- build.rs
+++ build.rs
@@ -4,25 +4,25 @@ use graphql_client_codegen::{
 };
 use std::fs::File;
 use std::io::{BufWriter, Write as _};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use syn::Token;
 
 const GH_PUBLIC_SCHEMA_URL: &'static str =
     "https://developer.github.com/v4/public_schema/schema.public.graphql";
 const SCHEMA_DOWNLOAD_PATH: &'static str = "graphql/schema.public.graphql";
 
-const OUTPUT_DIRECTORY_PATH: &'static str = "src/graphql/";
-
 // Queries
 const REPO_BASIC_INFO: &'static str = "graphql/query/repo_basic_info.graphql";
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     println!("cargo:rerun-if-changed={}", REPO_BASIC_INFO);
-    download_schema();
-    generate_code(&REPO_BASIC_INFO);
+    download_schema()?;
+    generate_code(&REPO_BASIC_INFO)?;
+    println!("cargo:rerun-if-changed=build.rs");
+    Ok(())
 }
 
-fn generate_code(query_path: &str) {
+fn generate_code(query_path: &str) -> anyhow::Result<()> {
     let query_path = PathBuf::from(query_path);
     let mut options = GraphQLClientCodegenOptions::new(CodegenMode::Cli);
     options.set_module_visibility(
@@ -33,7 +33,7 @@ fn generate_code(query_path: &str) {
     );
     let schema_path = PathBuf::from(SCHEMA_DOWNLOAD_PATH);
     let gen = generate_module_token_stream(query_path.clone(), &schema_path, options)
-        .expect("Module token stream generation failed!");
+        .expect("[build.rs] Module token stream generation failed!");
 
     let gen = quote::quote! {
       type DateTime = String;
@@ -43,49 +43,52 @@ fn generate_code(query_path: &str) {
 
     let generated_code = gen.to_string();
 
-    let query_file_name: ::std::ffi::OsString = query_path
+    let query_file_name = query_path
         .file_name()
         .map(ToOwned::to_owned)
-        .expect("Failed to find a file name in the provided query path.");
+        .ok_or_else(|| {
+            anyhow::anyhow!("[build.rs] Failed to find a file name in the provided query path.")
+        })?;
 
-    let dest_file_path: PathBuf = PathBuf::from(OUTPUT_DIRECTORY_PATH)
-        .join(query_file_name)
-        .with_extension("rs");
+    let out_dir = std::env::var_os("OUT_DIR").unwrap();
+    let dest_dir_path: PathBuf = Path::new(&out_dir).join("graphql");
+    if !dest_dir_path.exists() {
+        std::fs::create_dir_all(&dest_dir_path).unwrap();
+    }
+    let dest_file_path = dest_dir_path.join(query_file_name).with_extension("rs");
 
-    let mut file = File::create(dest_file_path).unwrap();
-    let file_write_err_msg = "Unable to write the generated code to file!";
-    writeln!(
-        file,
-        r###"// This is auto-generated using `build.rs` file! Do not modify!
-
-#![allow(clippy::redundant_static_lifetimes)]
-#![allow(unknown_lints)]
-"###
-    )
-    .expect(file_write_err_msg);
-    write!(file, "{}", generated_code).expect(file_write_err_msg);
+    let mut file = File::create(dest_file_path)?;
+    write!(file, "{}", generated_code)?;
+    Ok(())
 }
 
-fn download_schema() {
+fn download_schema() -> anyhow::Result<()> {
     // Check if schema file exists in path
     let schema_file_path = PathBuf::from(SCHEMA_DOWNLOAD_PATH);
     if !schema_file_path.exists() {
         // Download the file to the path!
-        let f = File::create(&schema_file_path).unwrap();
+        let f = File::create(&schema_file_path)?;
         let mut writer = BufWriter::new(f);
         let mut easy = Easy::new();
-        easy.url(&GH_PUBLIC_SCHEMA_URL).unwrap();
-        easy.write_function(move |data| Ok(writer.write(data).unwrap()))
-            .unwrap();
-        easy.perform().unwrap();
-        let response_code = easy.response_code().unwrap();
+        easy.url(&GH_PUBLIC_SCHEMA_URL)?;
+        easy.write_function(move |data| {
+            Ok(writer
+                .write(data)
+                .expect("[build.rs][curl] unable to write"))
+        })?;
+        easy.perform()?;
+        let response_code = easy.response_code()?;
         if response_code != 200 {
             panic!(
-                "Unexpected response code {} for {}",
+                "[build.rs][curl] Unexpected response code {} for {}",
                 response_code, GH_PUBLIC_SCHEMA_URL
             );
         }
     } else {
-        // Do nothing!
+        println!(
+            "[build.rs][curl] Found {}, Not downloading from internet!",
+            SCHEMA_DOWNLOAD_PATH
+        );
     }
+    Ok(())
 }
--- src/graphql/mod.rs
+++ src/graphql/mod.rs
@@ -1,1 +1,4 @@
-pub mod repo_basic_info;
+pub mod repo_basic_info {
+    // Include generated code, see `build.rs`!
+    include!(concat!(env!("OUT_DIR"), "/graphql/repo_basic_info.rs"));
+}
