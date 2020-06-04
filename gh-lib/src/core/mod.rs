00fbeaac974b553a275c9ba9ec01cda779576d8c
Aslam Ahammed <aslamplr@gmail.com>
2020-06-04 17:22:37 +0530

    Integrate actions/workflows
    - Integrate actions/workflows/* endpoints.
    - Add unit tests with mockito.

--- gh-lib/src/core/mod.rs
+++ gh-lib/src/core/mod.rs
@@ -1,3 +1,18 @@
+macro_rules! with_base_url {
+  ($($arg:tt)*) => ({
+      #[cfg(test)]
+      use mockito;
+
+      #[cfg(test)]
+      let url = format!("{}/{}", &mockito::server_url(), format!($($arg)*));
+      #[cfg(not(test))]
+      let url = format!("{}/{}", BASE_URL, format!($($arg)*));
+
+      url
+  })
+}
+
 pub mod basic_info;
 pub mod repos;
 pub mod secrets;
+pub mod workflows;
---
+++ gh-lib/src/core/workflows.rs
@@ -0,0 +1,291 @@
+use super::repos::RepoRequest;
+use crate::utils::http::get;
+use anyhow::Result;
+use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
+
+#[cfg(not(test))]
+const BASE_URL: &str = "https://api.github.com/repos";
+
+#[async_trait]
+pub trait Workflows {
+    async fn get_all_workflows(&self) -> Result<WorkflowList>;
+    async fn get_a_workflow(&self, workflow_id: i32) -> Result<Workflow>;
+    async fn get_workflow_usage(&self, workflow_id: i32) -> Result<WorkflowUsage>;
+}
+
+#[async_trait]
+impl Workflows for RepoRequest<'_> {
+    async fn get_all_workflows(&self) -> Result<WorkflowList> {
+        get_all_workflows(&self).await
+    }
+
+    async fn get_a_workflow(&self, workflow_id: i32) -> Result<Workflow> {
+        get_a_workflow(&self, workflow_id).await
+    }
+
+    async fn get_workflow_usage(&self, workflow_id: i32) -> Result<WorkflowUsage> {
+        get_workflow_usage(&self, workflow_id).await
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct WorkflowList {
+    pub total_count: i32,
+    pub workflows: Vec<Workflow>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Workflow {
+    pub id: i32,
+    pub node_id: String,
+    pub name: String,
+    pub path: String,
+    pub state: String,
+    pub created_at: chrono::DateTime<chrono::Utc>,
+    pub updated_at: chrono::DateTime<chrono::Utc>,
+    pub url: String,
+    pub html_url: String,
+    pub badge_url: String,
+}
+
+macro_rules! platform_usage {
+    (
+        $(
+            $(#[$docs:meta])*
+            $field:ident,
+        )+
+    ) => {
+        #[allow(non_snake_case)]
+        #[derive(Serialize, Deserialize, Debug, PartialEq)]
+        pub struct WorkflowUsagePlatform {
+            $(
+                $(#[$docs])*
+                pub $field: WorkflowUsageTiming,
+            )+
+        }
+    };
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct WorkflowUsage {
+    pub billable: WorkflowUsagePlatform,
+}
+
+platform_usage!(UBUNTU, MACOS, WINDOWS,);
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct WorkflowUsageTiming {
+    pub total_ms: i32,
+}
+
+async fn get_all_workflows(params: &RepoRequest<'_>) -> Result<WorkflowList> {
+    let RepoRequest(repo, auth_token) = params;
+    let url = with_base_url!("{}/actions/workflows", repo);
+    let resp = get(&url, &auth_token).await?;
+    let resp = resp.deserialize().await?;
+    Ok(resp)
+}
+
+async fn get_a_workflow(params: &RepoRequest<'_>, workflow_id: i32) -> Result<Workflow> {
+    let RepoRequest(repo, auth_token) = params;
+    let url = with_base_url!("{}/actions/workflows/{}", repo, workflow_id);
+    let resp = get(&url, &auth_token).await?;
+    let resp = resp.deserialize().await?;
+    Ok(resp)
+}
+
+async fn get_workflow_usage(params: &RepoRequest<'_>, workflow_id: i32) -> Result<WorkflowUsage> {
+    let RepoRequest(repo, auth_token) = params;
+    let url = with_base_url!("{}/actions/workflows/{}/timing", repo, workflow_id);
+    let resp = get(&url, &auth_token).await?;
+    let resp = resp.deserialize().await?;
+    Ok(resp)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito::{mock, Matcher};
+
+    #[tokio::test]
+    async fn get_all_workflows() -> Result<()> {
+        let repo_addr = "aslamplr/gh-cli";
+        let auth_token = "auth_secret_token";
+
+        let m = mock("GET", "/aslamplr/gh-cli/actions/workflows")
+            .match_header("Authorization", Matcher::Exact(format!("bearer {}", auth_token)))
+            .with_status(201)
+            .with_header("content-type", "application/json")
+            .with_body(r#"{
+                "total_count": 2,
+                "workflows": [
+                  {
+                    "id": 161335,
+                    "node_id": "MDg6V29ya2Zsb3cxNjEzMzU=",
+                    "name": "CI",
+                    "path": ".github/workflows/blank.yml",
+                    "state": "active",
+                    "created_at": "2020-01-08T23:48:37.000-08:00",
+                    "updated_at": "2020-01-08T23:50:21.000-08:00",
+                    "url": "https://api.github.com/repos/octo-org/octo-repo/actions/workflows/161335",
+                    "html_url": "https://github.com/octo-org/octo-repo/blob/master/.github/workflows/161335",
+                    "badge_url": "https://github.com/octo-org/octo-repo/workflows/CI/badge.svg"
+                  },
+                  {
+                    "id": 269289,
+                    "node_id": "MDE4OldvcmtmbG93IFNlY29uZGFyeTI2OTI4OQ==",
+                    "name": "Linter",
+                    "path": ".github/workflows/linter.yml",
+                    "state": "active",
+                    "created_at": "2020-01-08T23:48:37.000-08:00",
+                    "updated_at": "2020-01-08T23:50:21.000-08:00",
+                    "url": "https://api.github.com/repos/octo-org/octo-repo/actions/workflows/269289",
+                    "html_url": "https://github.com/octo-org/octo-repo/blob/master/.github/workflows/269289",
+                    "badge_url": "https://github.com/octo-org/octo-repo/workflows/Linter/badge.svg"
+                  }
+                ]
+              }"#)
+            .expect(1)
+            .create();
+
+        let expected_workflows = WorkflowList {
+            total_count: 2,
+            workflows: vec![
+                Workflow {
+                    id: 161335,
+                    node_id: "MDg6V29ya2Zsb3cxNjEzMzU=".into(),
+                    name: "CI".into(),
+                    path: ".github/workflows/blank.yml".into(),
+                    state: "active".into(),
+                    created_at: "2020-01-08T23:48:37.000-08:00".parse()?,
+                    updated_at: "2020-01-08T23:50:21.000-08:00".parse()?,
+                    url: "https://api.github.com/repos/octo-org/octo-repo/actions/workflows/161335"
+                        .into(),
+                    html_url:
+                        "https://github.com/octo-org/octo-repo/blob/master/.github/workflows/161335"
+                            .into(),
+                    badge_url: "https://github.com/octo-org/octo-repo/workflows/CI/badge.svg"
+                        .into(),
+                },
+                Workflow {
+                    id: 269289,
+                    node_id: "MDE4OldvcmtmbG93IFNlY29uZGFyeTI2OTI4OQ==".into(),
+                    name: "Linter".into(),
+                    path: ".github/workflows/linter.yml".into(),
+                    state: "active".into(),
+                    created_at: "2020-01-08T23:48:37.000-08:00".parse()?,
+                    updated_at: "2020-01-08T23:50:21.000-08:00".parse()?,
+                    url: "https://api.github.com/repos/octo-org/octo-repo/actions/workflows/269289"
+                        .into(),
+                    html_url:
+                        "https://github.com/octo-org/octo-repo/blob/master/.github/workflows/269289"
+                            .into(),
+                    badge_url: "https://github.com/octo-org/octo-repo/workflows/Linter/badge.svg"
+                        .into(),
+                },
+            ],
+        };
+
+        let repo_req = RepoRequest::try_from(repo_addr, auth_token)?;
+        let workflows = repo_req.get_all_workflows().await?;
+
+        m.assert();
+        assert_eq!(workflows, expected_workflows);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn get_a_workflow() -> Result<()> {
+        let repo_addr = "aslamplr/gh-cli";
+        let auth_token = "auth_secret_token";
+        let workflow_id = 161335;
+
+        let m = mock("GET", "/aslamplr/gh-cli/actions/workflows/161335")
+            .match_header("Authorization", Matcher::Exact(format!("bearer {}", auth_token)))
+            .with_status(201)
+            .with_header("content-type", "application/json")
+            .with_body(r#"{
+                    "id": 161335,
+                    "node_id": "MDg6V29ya2Zsb3cxNjEzMzU=",
+                    "name": "CI",
+                    "path": ".github/workflows/blank.yml",
+                    "state": "active",
+                    "created_at": "2020-01-08T23:48:37.000-08:00",
+                    "updated_at": "2020-01-08T23:50:21.000-08:00",
+                    "url": "https://api.github.com/repos/octo-org/octo-repo/actions/workflows/161335",
+                    "html_url": "https://github.com/octo-org/octo-repo/blob/master/.github/workflows/161335",
+                    "badge_url": "https://github.com/octo-org/octo-repo/workflows/CI/badge.svg"
+                  }"#)
+            .expect(1)
+            .create();
+
+        let expected_workflow = Workflow {
+            id: 161335,
+            node_id: "MDg6V29ya2Zsb3cxNjEzMzU=".into(),
+            name: "CI".into(),
+            path: ".github/workflows/blank.yml".into(),
+            state: "active".into(),
+            created_at: "2020-01-08T23:48:37.000-08:00".parse()?,
+            updated_at: "2020-01-08T23:50:21.000-08:00".parse()?,
+            url: "https://api.github.com/repos/octo-org/octo-repo/actions/workflows/161335".into(),
+            html_url: "https://github.com/octo-org/octo-repo/blob/master/.github/workflows/161335"
+                .into(),
+            badge_url: "https://github.com/octo-org/octo-repo/workflows/CI/badge.svg".into(),
+        };
+
+        let repo_req = RepoRequest::try_from(repo_addr, auth_token)?;
+        let workflows = repo_req.get_a_workflow(workflow_id).await?;
+
+        m.assert();
+        assert_eq!(workflows, expected_workflow);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn get_workflow_usage() -> Result<()> {
+        let repo_addr = "aslamplr/gh-cli";
+        let auth_token = "auth_secret_token";
+        let workflow_id = 161335;
+
+        let m = mock("GET", "/aslamplr/gh-cli/actions/workflows/161335/timing")
+            .match_header(
+                "Authorization",
+                Matcher::Exact(format!("bearer {}", auth_token)),
+            )
+            .with_status(201)
+            .with_header("content-type", "application/json")
+            .with_body(
+                r#"{
+                "billable": {
+                  "UBUNTU": {
+                    "total_ms": 180000
+                  },
+                  "MACOS": {
+                    "total_ms": 240000
+                  },
+                  "WINDOWS": {
+                    "total_ms": 300000
+                  }
+                }
+              }"#,
+            )
+            .expect(1)
+            .create();
+
+        let expected_usage = WorkflowUsage {
+            billable: WorkflowUsagePlatform {
+                UBUNTU: WorkflowUsageTiming { total_ms: 180000 },
+                MACOS: WorkflowUsageTiming { total_ms: 240000 },
+                WINDOWS: WorkflowUsageTiming { total_ms: 300000 },
+            },
+        };
+
+        let repo_req = RepoRequest::try_from(repo_addr, auth_token)?;
+        let usage = repo_req.get_workflow_usage(workflow_id).await?;
+
+        m.assert();
+        assert_eq!(usage, expected_usage);
+        Ok(())
+    }
+}
